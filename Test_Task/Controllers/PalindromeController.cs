2d9d3bd47aa60ae33666d3df3d3e88c08b427724
Sar-dim <sarancev.vrn@gmail.com>
2022-03-17 15:54:26 +0300

    Test done

---
+++ Test_Task/Controllers/PalindromeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace Test_Task.Controllers
+{
+    [ApiController]
+    [Route("palindrome")]
+    public class PalindromeController : Controller
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(LinkedList<int>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<bool> PalindromeCheck(string palindrome)
+        {
+            if (String.IsNullOrWhiteSpace(palindrome))
+            {
+                throw new ArgumentException("String shouldn't be null or empty");
+            }
+
+            var reverseString = String.Empty;
+
+            for (int i = palindrome.Length - 1; i >= 0; i--)
+            {
+                reverseString += palindrome[i].ToString();
+            }
+
+            if (reverseString == palindrome)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
